f869dadc16683a296e07398ee7e6da081d22c599
JDKurb <jared.d.kurber@gmail.com>
2024-03-30 12:48:36 -0500

    update build script

--- build-it-all.sh
+++ build-it-all.sh
@@ -1,11 +1,12 @@
 terraform init
 terraform apply --auto-approve
 aws eks update-kubeconfig --region us-east-2 --name jenkins-eks-cluster
-aws ecr create-repository --repository-name custom-jenkins --region us-east-2
 cd jenkins
 sudo service docker start
-docker build -t custom-jenkins .
-docker tag custom-jenkins:latest 905418215051.dkr.ecr.us-east-2.amazonaws.com/demo-repository
-docker push 905418215051.dkr.ecr.us-east-2.amazonaws.com/demo-repository
+sudo aws ecr create-repository --repository-name custom-jenkins --region us-east-2
+aws ecr get-login-password | sudo docker login --username AWS --password-stdin 905418215051.dkr.ecr.us-east-2.amazonaws.com
+sudo docker build -t custom-jenkins .
+sudo docker tag custom-jenkins:latest 905418215051.dkr.ecr.us-east-2.amazonaws.com/custom-jenkins:latest
+sudo docker push 905418215051.dkr.ecr.us-east-2.amazonaws.com/custom-jenkins:latest
 helm install -f values.yaml auto-jenkins oci://registry-1.docker.io/bitnamicharts/jenkins //Registry location needs to change
 export SERVICE_IP=$(kubectl get svc --namespace default auto-jenkins --template "{{ range (index .status.loadBalancer.ingress 0) }}{{ . }}{{ end }}") && echo "Jenkins URL: http://$SERVICE_IP/"
